fbfdda1663a55c4ad409ea54810b7bbeba0ceb89
Bojan_S <bojanspasovski1@gmail.com>
2021-09-21 20:09:07 +0200

    initial deploy commit

---
+++ precache-manifest.69ad86d3c50879683a679067753fd470.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce750a94fa0f90eea038",
+    "url": "/sticky-notes-app-deploy/static/css/main.b4e0a372.chunk.css"
+  },
+  {
+    "revision": "ce750a94fa0f90eea038",
+    "url": "/sticky-notes-app-deploy/static/js/main.fc17da56.chunk.js"
+  },
+  {
+    "revision": "4e3170bd614b6d71f8c7",
+    "url": "/sticky-notes-app-deploy/static/js/runtime~main.eb557b15.js"
+  },
+  {
+    "revision": "68ae1a02eb38e2b41261",
+    "url": "/sticky-notes-app-deploy/static/js/2.c3b02ecb.chunk.js"
+  },
+  {
+    "revision": "ad78827dbb116661810553275afafbaa",
+    "url": "/sticky-notes-app-deploy/index.html"
+  }
+];
